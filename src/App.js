40f2dec513f147eb0f49125f24f2bbc735c16a58
pablojr17 <pablojr17@gmail.com>
2021-02-07 19:26:45 -0300

    shadow

--- src/App.js
+++ src/App.js
@@ -2,36 +2,34 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import api from './services/api';
-import Typist from 'react-typist';
-import TypistLoop from 'react-typist-loop'
+import chuva from './assets/chuva.png'
+import sol from './assets/sol.png'
 
 function App() {
   const [City, setCity] = useState(null)
-
+  console.log(City)
   const id = 456777;
-  const dados = `weather?format=json-cors&key=0c2a0623&woeid=`
+  const dados = `weather?format=json-cors&key=2347a232&woeid=`
   const r = dados + id
-// useEffect(() => {
-//   async function loadTransactions() {
-//     const { data } = await api.get(`/${r}`);
-//     setCity(data.results.forecast)
-//   }
-//   loadTransactions()
-// }, [r]);
 
+useEffect(() => {
+  async function loadTransactions() {
+    const { data } = await api.get(`/${r}`);
+    setCity(data.results.forecast)
+  }
+  loadTransactions()
+}, [r]);
 
   return (
     <div className="App">
       <Header />
-
-      <a target="_blank" href="https://console.hgbrasil.com/documentation/weather/tools">Busque o codigo de sua Cidade</a>
-     <Typist startDelay={1000}>Weather Now</Typist>)
-
+      <h2>Previsão do tempo agora em:</h2>
+       <h1 className="city">Cotegipe - BA</h1>
       
-      {/* {City ? City.map((res, i) => {
+      {City ? City.map((res, i) => {
         return (
           <div className="item" key={i}>
-          <h1>{res.description}</h1>
+          <h1 className="titulo">{res.description}</h1>
           <div className="data">
             <p>{res.weekday}</p>
             <p>{res.date}</p>
@@ -40,119 +38,9 @@ function App() {
             <strong>Minima: {res.min}°</strong>
             <strong>Máxima: {res.max}°</strong>
           </div>
-
-        </div> */}
-          <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-            <div className="item">
-            <h1>description</h1>
-            <div className="data">
-              <p>weekday</p>
-              <p>date</p>
-            </div>
-            <div className="temp">
-              <strong>Minima: min°</strong>
-              <strong>Máxima: max°</strong>
-            </div>
-            </div>
-      {/* )}) : null} */}
+            <img className="chuva" src={chuva} alt=""/>
+          </div>
+            )}) : null}
     </div>
   );
 }
